b17e1cf4ee8bf334e3255d782a48b2d005639199
Sean Wang <wzx411098871@gmail.com>
2020-04-19 22:50:03 -0400

    add kitchen change scene & back to main scene

---
+++ test/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string sceneName;
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            PolygonCollider2D collider2D = this.gameObject.GetComponent<PolygonCollider2D>();
+
+            if (collider2D.OverlapPoint(mousePosition))
+            {
+                LoadScene();
+            }
+        }
+    }
+
+}
