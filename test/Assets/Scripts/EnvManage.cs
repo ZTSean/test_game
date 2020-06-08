1fcf58422fa89b7bc2d4b6a842008b032327f3af
Sean Wang <wzx411098871@gmail.com>
2020-06-07 21:47:10 -0400

    main scene load based on player level

--- test/Assets/Scripts/EnvManage.cs
+++ test/Assets/Scripts/EnvManage.cs
@@ -64,7 +64,6 @@ public class EnvManage : MonoBehaviour
             default:
                 Debug.LogError("No correct scene found");
                 break;
-
         }
         LoadEnvironmentObjects(currentSceneLevel);
     }
---
+++ test/Assets/Scripts/MainEnvManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainEnvManager : MonoBehaviour
+{
+    public Player player;
+    public GameObject cafeLevel1;
+    public GameObject cafeLevel2;
+    public GameObject factoryLevel1;
+    public GameObject factoryLevel2;
+    public GameObject dormLevel1;
+    public GameObject dormLevel2;
+    public GameObject spaceshipLevel1;
+    public GameObject spaceshipLevel2;
+
+    void Start()
+    {
+        LoadEnvironmentAll();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void LoadEnvironmentAll()
+    {
+        switch (player.playerData.cafeLevel)
+        {
+            case 0:
+                cafeLevel1.SetActive(true);
+                cafeLevel1.SetActive(false);
+                break;
+            case 1:
+                cafeLevel2.SetActive(true);
+                cafeLevel2.SetActive(false);
+                break;
+        }
+
+        switch (player.playerData.factoryLevel)
+        {
+            case 0:
+                factoryLevel1.SetActive(true);
+                factoryLevel2.SetActive(false);
+                break;
+            case 1:
+                factoryLevel2.SetActive(true);
+                factoryLevel1.SetActive(false);
+                break;
+        }
+
+        switch (player.playerData.dormLevel)
+        {
+            case 0:
+                dormLevel1.SetActive(true);
+                dormLevel2.SetActive(false);
+                break;
+            case 1:
+                dormLevel2.SetActive(true);
+                dormLevel1.SetActive(false);
+                break;
+        }
+
+        switch (player.playerData.level)
+        {
+            case 0:
+                spaceshipLevel1.SetActive(true);
+                spaceshipLevel2.SetActive(false);
+                break;
+            case 1:
+                spaceshipLevel2.SetActive(true);
+                spaceshipLevel1.SetActive(false);
+                break;
+        }
+    }
+}
