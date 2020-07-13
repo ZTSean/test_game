74c4f5966159392467d7594fcfcf7a2f8ba88023
Sean Wang <wzx411098871@gmail.com>
2020-07-12 20:01:07 -0400

    add working level 2 animation

--- test/Assets/Scripts/Player/Player.cs
+++ test/Assets/Scripts/Player/Player.cs
@@ -10,8 +10,8 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Load player data");
         playerData = SaveSystem.LoadPlayer();
+        Debug.Log("Load player data: " + playerData.lastUpdateTime);
     }
 
     private void OnDestroy()
